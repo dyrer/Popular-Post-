a77f168d225ac2b7447f3abdf5aa5a683468f999
Evangelos Athanasiadis <dyrer@hyperwebhost.net>
2015-03-18 11:40:56 +0200

    First commit

---
+++ popular-posts.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Plugin Name: Popular Posts
+ * Plugin URI: http://athanasiadis.me
+ * Description: A simple plugiin
+ * Version: The plugin's version number. Example: 1.0.0
+ * Author: Name of the plugin author
+ * Author URI: http://URI_Of_The_Plugin_Author
+ * Text Domain: Optional. Plugin's text domain for localization. Example: mytextdomain
+ * Domain Path: Optional. Plugin's relative directory path to .mo files. Example: /locale/
+ * Network: Optional. Whether the plugin can only be activated network wide. Example: true
+ * License: GPL2
+ */
+
+ /**
+  * Post Popoularity Counter
+  */
+  
+  
+  
+  
+  /**
+   * Dynamically inject counter into single posts
+   */
+  
+  function my_counter_popular_posts ($post_id) {
+    // Check that this is a single post and that the user is a visitor
+    if ($is_single()) return;
+    if ($is_user_logged_in()) {
+        // Get the post ID
+        // Run the post Popularity Counter on post
+    }
+  }
+ 
